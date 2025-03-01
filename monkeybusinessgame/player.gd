063fd764e5b4bacb21c033c9e3e58e99e340eeaa
Gabe Zeller <gabeaz03@gmail.com>
2025-03-01 00:52:46 -0600

    Added play walking mechanics and animations

---
+++ monkeybusinessgame/player.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+var lastDir = "s_idle"
+@onready var animation = $AnimatedSprite2D
+const SPEED = 100
+
+
+func _ready() -> void:
+	pass
+
+
+
+func _process(delta: float) -> void:
+	var direction = Input.get_vector("left","right","up","down")
+	velocity = direction * SPEED
+	move_and_slide()
+	
+	var x = direction.x
+	var y = direction.y
+	
+	if x > 0 and y == 0: # east
+		animation.play("e_walk")
+		lastDir = "e_idle"
+	elif x == 0 and y > 0: #south
+		animation.play("s_walk")
+		lastDir = "s_idle"
+	elif x < 0 and y == 0: #west
+		animation.play("w_walk")
+		lastDir = "w_idle"
+	elif x == 0 and y < 0: #north
+		animation.play("n_walk")
+		lastDir = "n_idle"
+	elif x > 0 and y > 0: #south east
+		animation.play("se_walk")
+		lastDir = "se_idle"
+	elif x < 0 and y < 0: # north west
+		animation.play("nw_walk")
+		lastDir = "nw_idle"
+	elif x > 0 and y < 0: #north east
+		animation.play("ne_walk")
+		lastDir = "ne_idle"
+	elif x < 0 and y > 0: # south west
+		animation.play("sw_walk")
+		lastDir = "sw_idle"
+	else:
+		animation.play(lastDir)
+		
+	
+
+	
